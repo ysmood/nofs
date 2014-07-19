6fc6412491a394ad8f02cc585530d4f0ffa02d2f
Yad Smood <y.s.inside@gmail.com>
2014-07-19 16:37:52 +0800

    add: basic

---
+++ test/basic.js
@@ -0,0 +1,70 @@
+require('../lib/main');
+var assert = require('assert');
+
+describe('Basic:', function () {
+	it('should work', function (tdone) {
+		fs.existsQ('readme.md').done(function (ret) {
+			try {
+				assert.equal(ret, true);
+				tdone();
+			} catch (e) {
+				tdone(e);
+			}
+		});
+	});
+
+	it('should work', function (tdone) {
+		fs.dirExistsQ('lib').done(function (ret) {
+			try {
+				assert.equal(ret, true);
+				tdone();
+			} catch (e) {
+				tdone(e);
+			}
+		});
+	});
+
+	it('should work', function (tdone) {
+		fs.dirExistsQ('readme.md').done(function (ret) {
+			try {
+				assert.equal(ret, false);
+				tdone();
+			} catch (e) {
+				tdone(e);
+			}
+		});
+	});
+
+	it('should work', function (tdone) {
+		fs.fileExistsQ('readme.md').done(function (ret) {
+			try {
+				assert.equal(ret, true);
+				tdone();
+			} catch (e) {
+				tdone(e);
+			}
+		});
+	});
+
+	it('should work', function (tdone) {
+		fs.fileExistsQ('lib').done(function (ret) {
+			try {
+				assert.equal(ret, false);
+				tdone();
+			} catch (e) {
+				tdone(e);
+			}
+		});
+	});
+
+	it('should work', function (tdone) {
+		fs.readFileQ('test/sample.txt', 'utf8').done(function (ret) {
+			try {
+				assert.equal(ret, 'test');
+				tdone();
+			} catch (e) {
+				tdone(e);
+			}
+		});
+	});
+});
