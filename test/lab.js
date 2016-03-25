aa11c2729f31eb4224e1e49dfc2888a2411ed76d
Yad Smood <y.s.inside@gmail.com>
2016-03-25 12:34:09 +0800

    v0.11.2

--- test/lab.js
+++ test/lab.js
@@ -2,12 +2,4 @@ var Promise, nofs, npath;
 
 nofs = require('../src/main');
 
-npath = require('path');
-
-Promise = require('../src/utils').Promise;
-
-nofs.watchPath('test/basic.js', {
-  handler: function(path) {
-    return console.log(path);
-  }
-});
+nofs.remove('.nokit')
